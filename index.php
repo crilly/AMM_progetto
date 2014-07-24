1efe09ef5e6aff40f5de36861a270c22d2e4d528
Crily <Crily@192.168.56.1>
2014-07-24 10:22:19 +0200

    primo file

---
+++ index.php
@@ -0,0 +1,56 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>   
+        <meta http-equip="Content-Type"  content="text/html">    
+        <meta charset="UTF-8">
+        <title>VideotecaOnline</title> 
+        <link rel="stylesheet" type="text/css" href="css/stile.css"> 
+    </head>
+
+    <body>
+        <div class="body">
+            <div class="header">
+                <img src="immagini/titolo.png" alt="VideotecaOnline" id="logo"/> 
+                <div class="input">
+                    <h1>Login</h1>
+                    <form method="post" action="docente_reg_esami.html">       
+                        <label for="username">Username</label> 
+                        <input name="username" id="username" type="text"/>
+                        <br/> <br/>
+                        <label for="password">Password </label> 
+                        <input name="password" id="password" type="password"/>
+                        <br/> <br/>  
+                        <input type="submit" value="Accedi"/>  
+                    </form> 
+                </div>
+            </div>
+
+            <div class="container">
+                <div id="menu">
+                    <ul>
+                        <li> <a href="source.html">Home</a></li>
+                        <li> <a href="info.html">Info</a></li>
+                        <li> <a href="source.html">Lista Film</a></li>
+                        <li> <a href="source.html">Carrello</a></li>
+                        <li> <a href="source.html">Ricerca</a></li>
+                    </ul>
+                </div>
+                    
+                <div class="container-body"></div>
+            </div>
+        </div>
+        
+        <div class="footer">
+            
+        </div>
+    
+</body>
+
+</html>
