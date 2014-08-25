1d2ce9a98b04daf3cd3b1022010fadba76094bb2
Crily <Crily@Cristin>
2014-08-25 17:22:50 +0200

    modifiche settings

--- php/settings.php
+++ php/settings.php
@@ -19,18 +19,21 @@ class Settings {
      * Lo uso perche' la mia configurazione locale e' ovviamente diversa da quella
      * pubblica. Gestisco il problema una volta per tutte in questo script
      */
+    
     public static function getApplicationPath() {
         if (!isset(self::$appPath)) {
-// restituisce il server corrente
+            // restituisce il server corrente
             switch ($_SERVER['HTTP_HOST']) {
                 case 'localhost':
-// configurazione locale
-                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/ammproject/';
+                    // configurazione locale
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/AMM_progetto/';
                     break;
+                
                 case 'spano.sc.unica.it':
-// configurazione pubblica
+                // configurazione pubblica
                     self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2014/sannaCristin/';
                     break;
+                
                 default:
                     self::$appPath = '';
                     break;
@@ -38,5 +41,4 @@ class Settings {
         }
         return self::$appPath;
     }
-
 }
