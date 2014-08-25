ff22e36d7971dc35704273233cbc70dcdf9fc12e
Crily <Crily@Cristin>
2014-08-25 11:31:20 +0200

    prove sql

--- tabelleSQL.sql
+++ tabelleSQL.sql
@@ -1,3 +1,5 @@
+-- tabella clienti --
+
 CREATE TABLE IF NOT EXISTS `clienti` (
 `id` int(10) NOT NULL AUTO_INCREMENT,
 `nome` varchar(20) DEFAULT NULL,
@@ -6,3 +8,30 @@ CREATE TABLE IF NOT EXISTS `clienti` (
 `password` varchar(15) DEFAULT NULL,
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+-- tabella film -- 
+
+CREATE TABLE IF NOT EXISTS `film` (
+`id` int(10) NOT NULL AUTO_INCREMENT,
+`titolo` varchar(20) DEFAULT NULL,
+`regista` varchar(20) DEFAULT NULL,
+`anno` int(4) DEFAULT NULL,
+`genere` varchar(20) DEFAULT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+-- tabella noleggi --
+
+CREATE TABLE IF NOT EXISTS `noleggi` (
+`id` int(10) NOT NULL AUTO_INCREMENT,
+`idfilm` int(10) DEFAULT NULL,
+`idcliente` int(11) DEFAULT NULL,
+`datainizio` datetime DEFAULT NULL,
+`datafine` date DEFAULT NULL,
+PRIMARY KEY (`id`),
+KEY `film_fk` (`idfilm`),
+KEY `cliente_fk` (`idcliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
