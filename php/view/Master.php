011e399c2c8e0453ffe69dee2e45803906df3bf0
Crily <Crily@Cristin>
2014-08-29 16:23:22 +0200

    master

---
+++ php/view/Master.php
@@ -0,0 +1,46 @@
+<?php
+include_once 'ViewDescriptor.php';
+include_once basename(__DIR__) . '/../Settings.php';
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title><?= $vd->getTitolo() ?></title>
+        <base href="<?= Settings::getApplicationPath() ?>php/"/>
+        <link href="../css/stile.css" rel="stylesheet" type="text/css"/>
+        <link type="image/x-icon" href="../immagini/titolo.png" />
+        <!--<script type="text/javascript" src="../js/ajax.js"></script>-->
+    </head>
+    <body>
+        <div id="page">
+            
+            <div id="header">
+                <?php
+                $logo = $vd->getLogoFile();
+                require "$logo";
+                ?>
+            </div>
+            <div id="menu">
+                <?php
+                $menu = $vd->getMenuFile();
+                require "$menu";
+                ?>
+            </div>
+
+            <div id="content">
+
+                <?php
+                $content = $vd->getContentFile();
+                require "$content";
+                ?>
+            </div>
+            <div id="footer">
+                <a id="WikipediaFilm" href="https://it.wikipedia.org/wiki/Film">Wikipedia Film</a> 
+                <a id="ComingSoon" href="http://www.comingsoon.it/cinema/filmalcinema/">Film al cinema</a>
+                <h6>Progetto di Amministrazione di Sistema</br>di Cristin Sanna</h6>
+            </div>
+        </div>
+    </body>
+</html>
