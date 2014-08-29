9f28e5d791d51695c79112ffe2ce6ae7d36f4f70
Crily <Crily@Cristin>
2014-08-29 17:12:19 +0200

    master

--- php/view/login/content.php
+++ php/view/login/content.php
@@ -1,8 +1,14 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div class="input-form">
+    
+    <h3>Login</h3>
+    <form method="post" action="login">
+        <input type="hidden" name="cmd" value="login"/>
+        <label for="user">Username</label>
+        <input type="text" name="user" id="user"/>
+        <br>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password"/>
+        <br/>
+        <input type="submit" value="Login"/>
+    </form>
+</div>
