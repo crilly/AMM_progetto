93afca7a8d11a08907050c9805b8c137dfd5f4a9
Crily <Crily@Cristin>
2014-08-29 18:05:41 +0200

    menu

--- php/view/login/menu.php
+++ php/view/login/menu.php
@@ -1,8 +1,4 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<h3>Menu</h3>
+<ul>
+    <li><a href="#">Home</a></li>
+</ul>
