1b3c3c61fa45ae56a7f559becd92b4e976a848c1
Crily <Crily@Cristin>
2014-08-29 13:06:12 +0200

    implementazione venditore

--- php/model/Venditore.php
+++ php/model/Venditore.php
@@ -1,8 +1,21 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include_once 'User.php';
 
+/**
+ * Classe che rappresenta un cliente
+ */
+class Cliente extends User {
+
+    /**
+     * Costruttore della classe
+     */
+    public function __construct() {
+    //Richiamiamo il costruttore della superclasse
+        parent::__construct();
+        $this->setRuolo(User::Venditore);
+    }
+
+}
+
+?>
