b31a6884f0fb7903ace6f68cdc9c02399f588a42
Crily <Crily@Cristin>
2014-09-01 17:59:14 +0200

    modifiche clientecontroller

--- php/controller/ClienteController.php
+++ php/controller/ClienteController.php
@@ -1,15 +1,30 @@
 <?php
 
 include_once 'BaseController.php';
+include_once basename(__DIR__) . '/../model/UserFactory.php';
+include_once basename(__DIR__) . '/../model/Dvd.php';
+include_once basename(__DIR__) . '/../model/DvdFactory.php';
 
+/**
+ * Controller del cliente
+ */
 class ClienteController extends BaseController {
 
+    /**
+     * Costruttore
+     */
     public function __construct() {
         parent::__construct();
     }
 
+    /**
+     * Metodo per gestire l'input dell'utente.
+     * @param type $request la richiesta da gestire
+     */
     public function handleInput(&$request) {
+        //Creo il descrittore della vista
         $vd = new ViewDescriptor();
+        //Imposto la pagina
         $vd->setPagina($request['page']);
         if (!$this->loggedIn()) {
             //Utente non autenticato, rimando alla home
@@ -18,27 +33,42 @@ class ClienteController extends BaseController {
             //Utente autenticato
             $user = UserFactory::instance()->cercaUtentePerId(
                     $_SESSION[BaseController::user], $_SESSION[BaseController::role]);
+            // verifico quale sia la sottopagina della categoria
+            // Cliente da servire ed imposto il descrittore
+            // della vista per caricare i "pezzi" delle pagine corretti
+            // tutte le variabili che vengono create senza essere utilizzate
+            // direttamente in questo switch, sono quelle che vengono poi lette
+            // dalla vista, ed utilizzano le classi del modello
+            if (isset($request["subpage"])) {
+                switch ($request["subpage"]) {
+                    // visualizzazione dei noleggi richiesti
+                    //visualizzazione dell'elenco dei film
+                    case 'film':
+                        $film = FilmFactory::instance()->getFilm();
+                        $vd->setSottoPagina('ListaFilm');
+                        break;
+                    default:
+                        $vd->setSottoPagina('Home');
+                        break;
+                }
+            }
             //Gestione dei comandi inviati dall'utente
             if (isset($request["cmd"])) {
+                //Abbiamo ricevuto un comando
                 switch ($request["cmd"]) {
-                    
+                    //Logout
                     case 'logout':
                         $this->logout($vd);
                         break;
-                    
-                    default:
-                        $this->showHomeUtente($vd);
-                        break;
                 }
-            } else {
-                //Nessun comando
-                $user = UserFactory::instance()->cercaUtentePerId(
-                        $_SESSION[BaseController::user], $_SESSION[BaseController::role]);
-                $this->showHomeUtente($vd);
             }
         }
-        //Richiamo la vista
+
+
+        //Includo la vista
         require basename(__DIR__) . '/../view/master.php';
     }
 
 }
+
+?>
