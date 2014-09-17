6409df4d79028a2c6fcf398395e292c07ed38300
Crily <Crily@Cristin>
2014-09-17 18:35:21 +0200

    prova

--- config.php
+++ config.php
@@ -1,9 +1,15 @@
 <?php 
+    
+    
+   $Host="localhost"; //Nome Host.
+$User="sannaCristin"; //Username PHP-MyAdmin.
+$Password="criceto846"; //PassWord.
+$db="amm14_sannaCristin"; //Nome DataBase.
 
-$Host="localhost"; //Nome Host.
+/* $Host="localhost"; //Nome Host.
 $User="admin"; //Username PHP-MyAdmin.
 $Password="admin"; //PassWord.
-$db="videoteca"; //Nome DataBase.
+$db="videoteca"; //Nome DataBase. */
 
 $conn=mysqli_connect($Host,$User,$Password); //Effettua la connessione.
 $r=mysqli_select_db($conn,$db); //Seleziona il database.
