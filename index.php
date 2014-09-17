4e17f996dfec100ff6bfd81df1d29cf9ddb5287e
Crily <Crily@Cristin>
2014-09-17 18:55:25 +0200

    prova

--- index.php
+++ index.php
@@ -1,34 +1,66 @@
-<?php
-session_start(); 
-include("config.php"); 
-
-
-include("header.php");  
-   
- include("testoiniziale.php"); ?>
-      
-      ?>
-      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula
-       eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient 
-       montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
-        pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla
-         vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a,
-          venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.
-           Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. 
-           Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim
-           . Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-            Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. A
-            enean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorp
-            er ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tell
-            us eget condimentum rhoncus, sem quam semper libero, sit amet adipiscin
-            g sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendr
-            erit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapi
-            en ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget ero
-            s faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodale
-            s sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
-      <?php
-  
-include("down.php");   
- 
-
-               ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Progetto di Amministrazione di Sistema</title>
+    </head>
+    <body>
+        <h1>Link al progetto</h1>
+        <p>
+            <a href="http://spano.sc.unica.it/amm2014/sannaCristin/site">Login</a>
+        </p>
+        <p>
+        <h3>Descrizione</h3>
+        L'applicazione simula una videoteca dalla quale è possibile noleggiare dei film. Gli utenti sono divisi in amministratori e clienti.</br>
+        I dipendenti possono:
+        <ul>
+            <li>Visualizzare il parco auto</li>
+            <li>Aggiungere veicoli al parco auto</li>
+            <li>Rimuovere veicoli dal parco auto</li>
+            <li>Visualizzare lo storico prenotazioni di tutti gli utenti</li>
+            <li>Gestire la propria anagrafica</li>
+        </ul>
+        I clienti possono:
+        <ul>
+            <li>Visualizzare il parco auto</li>
+            <li>Registrare un noleggio</li>
+            <li>Visualizzare lo storico dei propri noleggi</li>
+        </ul>
+    </p>
+    <p>
+    <h3>Note</h3>
+    <ul>
+        <li>Le date sono nel formato Y-m-d</li>
+        <li>Il costo giornaliero è una caratteristica del modello di veicolo, non del singolo veicolo</li>
+    </ul>
+</p>
+<p>
+<h3>Requisiti soddisfatti</h3>
+<ol>
+    <li>Utilizzo di HTML e CSS</li>
+    <li>Utilizzo di PHP e MySQL</li>
+    <li>Utilizzo del pattern MVC</li>
+    <li>Dueue ruoli (cliente e dipendente)</li>
+    <li>Transazione per il salvataggio di un nuovo noleggio (metodo nuovo della classe NoleggioFactory)</li>
+    <li>Caricamento ajax dei risultati filtrati dello storico dei noleggi (ruolo dipendente)</li>
+</ol>
+</p>
+<p>
+<h3>Utenti</h3>
+<ul>
+    <li>Dipendente
+        <ul>
+            <li>username: stefano</li>
+            <li>passowrd: stefano</li>
+        </ul>
+    </li>
+    <li>Cliente
+        <ul>
+            <li>username: davide</li>
+            <li>passowrd: davide</li>
+        </ul>
+    </li>
+</ul>
+</p>
+</body>
+</html>
